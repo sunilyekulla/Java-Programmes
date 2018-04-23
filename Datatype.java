64d0c7678009d8bda80b1835006667262801c038
sunilyekulla <38101561+sunilyekulla@users.noreply.github.com>
2018-04-23 15:37:11 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,27 @@
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		byte b1 = 21; 
+		short shortnumber = 4239;
+		int i = 1222222222;
+		long longnumber = 1414234123;
+		float floatingnumber = 2.5f;
+		Double number = 121.00;
+		boolean bool1 = true;
+		char char1 = 'S';
+		
+		
+		System.out.println("Byte number is:" + b1);
+		System.out.println("short number is:" + shortnumber);
+		System.out.println("Integer is:" + i);
+		System.out.println("Long number is:" + longnumber);
+		System.out.println("Float number is:" + floatingnumber);
+		System.out.println("Double number is:" + number);
+		System.out.println("Boolean value is:" + bool1);
+		System.out.println("Character value is:" + char1);
+				
+
+	}
+
+}
