74f161529fb5f6b70a1484efdfc922c51dfb00a2
sunilyekulla <38101561+sunilyekulla@users.noreply.github.com>
2018-04-24 15:45:52 +0530

    Add files via upload

---
+++ Ifelseif.java
@@ -0,0 +1,17 @@
+package Package1;
+
+public class Ifelseif {
+
+	public static void main(String[] args) {
+		int x = 0;
+		if (x < 0) {
+			System.out.println("Negative number");		
+		}else if(x > 0) {
+				System.out.println("Positive number");
+		}else {
+			System.out.println("Zero");
+		}
+				
+			}			
+
+	}
---
+++ conditional.java
@@ -0,0 +1,12 @@
+
+public class conditional {
+
+	public static void main(String[] args) {
+	  String weather = "rain was stop";
+	  if(weather=="raining"){
+	     System.out.println("I am staying in home");
+	  } else {
+	     System.out.println("I want to go out for walk");
+	  }
+	}
+}
